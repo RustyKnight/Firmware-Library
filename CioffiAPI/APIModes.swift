e34d93e77fc8d3613c4985150f1af730ded6c504
Shane Whitehead <shane.whitehead@beamcommunications.com>
2016-07-27 11:32:53 +1000

    Add API modes

---
+++ CioffiAPI/APIModes.swift
@@ -0,0 +1,15 @@
+//
+//  APIModes.swift
+//  CioffiAPI
+//
+//  Created by Shane Whitehead on 27/07/2016.
+//  Copyright © 2016 KaiZen. All rights reserved.
+//
+
+import Foundation
+
+public enum NetworkMode: Int {
+    case satellite = 0
+    case cellular = 1
+    case smartSwitch = 2
+}
